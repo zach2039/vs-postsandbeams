cb8b33dada62fece26511c16ad1631b5bcf8c37c
Zach2039 <sumyunguy2007@gmail.com>
2023-05-26 10:44:03 -0500

    Rework beams and posts
    Add config

---
+++ src/postsandbeams/PostAndBeamsConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostsAndBeams
+{
+    public class PostsAndBeamsConfig
+    {
+        public string unstableFallingSupportableDropChanceText = "Chance that blocks with behavior UnstableFallingSupportable can fall when a neighboring block is updated";
+        public float unstableFallingSupportableDropChance = 0.1f;
+
+        public string canUnstableFallingSupportableCascadeText = "Whether or not blocks with behavior UnstableFallingSupportable can cause similar blocks to fall without checking for support";
+        public bool canUnstableFallingSupportableCascade = true;
+
+        public string patchCrackedRockToApplyUnstableFallingSupportableText = "Patch cracked rock to apply behavior UnstableFallingSupportable";
+        public bool patchCrackedRockToApplyUnstableFallingSupportable = true;
+
+        public PostsAndBeamsConfig() { }
+
+        public static PostsAndBeamsConfig Current { get; set; }
+
+        public static PostsAndBeamsConfig GetDefault()
+        {
+            PostsAndBeamsConfig defaultConfig = new PostsAndBeamsConfig();
+
+            defaultConfig.unstableFallingSupportableDropChanceText.ToString();
+            defaultConfig.unstableFallingSupportableDropChance = 0.1f;
+
+            defaultConfig.canUnstableFallingSupportableCascadeText.ToString();
+            defaultConfig.canUnstableFallingSupportableCascade = true;
+
+            defaultConfig.patchCrackedRockToApplyUnstableFallingSupportableText.ToString();
+            defaultConfig.patchCrackedRockToApplyUnstableFallingSupportable = true;
+
+            return defaultConfig;
+        }
+    }
+}
--- src/postsandbeams/PostsAndBeamsCore.cs
+++ src/postsandbeams/PostsAndBeamsCore.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Vintagestory.API.Common;
 
 using PostsAndBeams.ModBlock;
+using PostsAndBeams.ModBlockBehavior;
 
 namespace PostsAndBeams
 {
@@ -18,6 +19,32 @@ namespace PostsAndBeams
             api.Logger.Notification("Loaded Posts And Beams!");
 
             api.RegisterBlockClass("BlockPost", typeof(BlockPost));
+            api.RegisterBlockBehaviorClass("BreakIfNotConnectedPost", typeof(BlockBehaviorBreakIfNotConnectedPost));
+            api.RegisterBlockBehaviorClass("UnstableFallingSupportable", typeof(BlockBehaviorUnstableFallingSupportable));
+
+            try
+            {
+                var Config = api.LoadModConfig<PostsAndBeamsConfig>("postsandbeams.json");
+                if (Config != null)
+                {
+                    api.Logger.Notification("Mod Config successfully loaded.");
+                    PostsAndBeamsConfig.Current = Config;
+                }
+                else
+                {
+                    api.Logger.Warning("No Mod Config specified. Falling back to default settings!");
+                    PostsAndBeamsConfig.Current = PostsAndBeamsConfig.GetDefault();
+                }
+            }
+            catch
+            {
+                PostsAndBeamsConfig.Current = PostsAndBeamsConfig.GetDefault();
+                api.Logger.Error("Failed to load custom mod configuration. Falling back to default settings!");
+            }
+            finally
+            {
+                api.StoreModConfig(PostsAndBeamsConfig.Current, "postsandbeams.json");
+            }
         }
     }
 }
